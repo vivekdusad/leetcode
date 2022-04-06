fcff98f416b52359b1d63d9e682cf3c6220a309e
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-04-06 22:48:30 +0530

    Time: 332 ms (51.70%), Space: 10.5 MB (74.83%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,50 +1,37 @@
 class Solution {
-    public int threeSumMulti(int[] A, int target) {
-        int MOD = 1_000_000_007;
-        long ans = 0;
-        Arrays.sort(A);
-
-        for (int i = 0; i < A.length; ++i) {
-            // We'll try to find the number of i < j < k
-            // with A[j] + A[k] == T, where T = target - A[i].
-
-            // The below is a "two sum with multiplicity".
-            int T = target - A[i];
-            int j = i+1, k = A.length - 1;
-
-            while (j < k) {
-                // These steps proceed as in a typical two-sum.
-                if (A[j] + A[k] < T)
-                    j++;
-                else if (A[j] + A[k] > T)
-                    k--;
-                else if (A[j] != A[k]) {  // We have A[j] + A[k] == T.
-                    // Let's count "left": the number of A[j] == A[j+1] == A[j+2] == ...
-                    // And similarly for "right".
-                    int left = 1, right = 1;
-                    while (j+1 < k && A[j] == A[j+1]) {
-                        left++;
-                        j++;
-                    }
-                    while (k-1 > j && A[k] == A[k-1]) {
-                        right++;
-                        k--;
-                    }
-
-                    ans += left * right;
-                    ans %= MOD;
-                    j++;
-                    k--;
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        int mod = 1e9+7;
+        long long ans=0;
+        int n = arr.size();
+        cout<<n<<endl;
+        sort(arr.begin(), arr.end());
+        for(int a=0;a<arr.size();a++)
+        {
+            int sum = target-arr[a];            
+            int i=a+1;
+            int j=n-1;
+            while(i<j){
+                int curr = arr[i]+arr[j];
+                if(curr<sum){
+                    i++;
+                }else if(curr>sum){
+                    j--;
+                }else if(arr[i] !=arr[j]){
+                    int count_a=1;
+                    int count_b=1;
+                    while(i+1<j and arr[i]==arr[i+1]) i++, count_a++;
+                    while(j-1>i and arr[j]==arr[j-1]) j--, count_b++;
+                    ans = (ans+(count_a*count_b));
+                    ans = ans%mod;
+                    i++, j--;
                 } else {
-                    // M = k - j + 1
-                    // We contributed M * (M-1) / 2 pairs.
-                    ans += (k-j+1) * (k-j) / 2;
-                    ans %= MOD;
+                    ans += (j-i+1) * (j-i) / 2;
+                    ans %= mod;
                     break;
                 }
             }
         }
-
         return (int) ans;
     }
-}
+};
