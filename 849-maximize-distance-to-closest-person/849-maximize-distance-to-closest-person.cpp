69a59521e3254c131386ac38a3d67f187e1f8a80
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-04-07 16:41:25 +0530

    Time: 15 ms (72.55%), Space: 18 MB (6.79%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -1,23 +1,28 @@
+typedef vector<int> vi;
 class Solution {
 public:
-    int maxDistToClosest(vector<int> &seats)
+    int maxDistToClosest(vi &seats)
     {
-        int i = 0;
-        int j = 1;
-        int n = seats.size();
-        int ans = INT_MIN;
-        while(seats[i]==0) i++, j++;
-        ans = max(ans, i);
-        while (j < n)
-        {
-            if (seats[j] == 1)
-            {
-                int mid = (j - i) / 2;
-                ans = max(ans, mid );
-                i = j;
-            }
-            j++;
+        int n =seats.size();
+        vi left(n,n);
+        vi right(n,n);
+        
+        for(int i=0;i<n;i++){
+            if(seats[i] ==1) left[i]=0;
+            else if(i>0) left[i] = left[i-1]+1;
         }
-        return max(ans, n-i-1);
+        
+        for(int i=n-1;i>=0;i--){
+            if(seats[i]==1) right[i]=0;
+            else if(i<n-1) right[i] = right[i+1]+1;
+        }
+        int ans =INT_MIN;
+        for(int i=0;i<n;i++){
+            if(seats[i]==0){
+             ans = max(ans,min(left[i],right[i]));
+            }
+        }
+        return ans;
+        
     }
 };
