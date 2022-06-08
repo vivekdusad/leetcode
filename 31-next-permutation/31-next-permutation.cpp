8125ead05b6355c2645677ef922a1b1c12cb44f4
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-06-08 15:40:00 +0530

    Time: 3 ms (87.89%), Space: 12.2 MB (37.66%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
+    void reverseBtwnIndexes(vector<int>& nums,int i,int j){
+        int start = i;
+        int end = j;
+        while(start<end){
+            swap(nums[start],nums[end]);
+            start++;
+            end--;
+        }
+    }
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i = n - 2;
-        while (i >= 0)
-        {
-            if (nums[i] >= nums[i + 1])
-            {
-                i--;
+        int i = n-2;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]>=nums[i+1]){
                 continue;
-            }
-            else
-            {
-                for (int j = n - 1; j > i; j--)
-                {
-                    if (nums[j] > nums[i])
-                    {
-                        swap(nums[i], nums[j]);
-                        break;
+            }else{
+                for(int j=n-1;j>i;j--){
+                    if(nums[i]<nums[j]){
+                        swap(nums[i],nums[j]);
+                        reverse(nums.begin()+i+1,nums.end());
+                        return;
                     }
                 }
-                reverse(nums.begin() + i + 1, nums.end());
-                return;
             }
         }
         sort(nums.begin(),nums.end());
-        return;
     }
+    
 };
