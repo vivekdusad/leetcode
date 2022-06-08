dcfeca4df5d8bfd9a0f8eb533a9b10eed6cca848
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-06-08 11:21:25 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (33.07%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,33 +3,24 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> result;
         vector<int> a ={1};
-        vector<int> b ={1,1};
-        vector<int> c = {1,2,1};
-        if(numRows>=1){
-            result.push_back(a);
+        result.push_back(a);                
+        for(int i=1;i<numRows;i++){
+            vector<int> temp;
+            temp.push_back(1);
+            for(int j=1;j<i;j++){            
+            temp.push_back(giveNcR(i,j));            
         }
-        if(numRows>=2){
-            result.push_back(b);
-        }
-        if(numRows>=3){
-            result.push_back(c);
-        }
-        if(numRows>3){
-            for(int i=3;i<numRows;i++){
-                vector<int> temp;
-                vector<int> last = result[result.size()-1];
-                int k=0;int j=1;
-                temp.push_back(1);
-                int n = last.size();
-                while(j<n){
-                    temp.push_back(last[k]+last[j]);
-                    k++;
-                    j++;
-                }
-                temp.push_back(1);
-                result.push_back(temp);
-            }
+            temp.push_back(1);
+            result.push_back(temp);
         }
         return result;
+    }
+    int giveNcR(int n,int k){
+        int res=1;
+        for(int i=0;i<k;i++){
+            res  *= (n-i);
+            res /= (i+1);
+        }
+        return res;
     }
 };
