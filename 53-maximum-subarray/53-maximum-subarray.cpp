335c7726901f864804b6e45cee5e3c14118f6715
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-06-09 11:05:33 +0530

    Time: 257 ms (5.09%), Space: 70.5 MB (6.16%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,13 +2,27 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_ele = nums[0],sum=0;
-        for(auto i:nums)
-        {
-            sum += i;
-            max_ele = max(max_ele,sum);
-            if(sum<0) sum=0;
+        vector<int> max_cache(nums.size());
+        max_cache[0] = nums[0];
+        for(int i=1;i<nums.size();i++){
+            max_cache[i] = max(nums[i]+max_cache[i-1],nums[i]);
         }
-        return max_ele;
+        int ans = max_cache[0];
+        for(int i=1;i<nums.size();i++){
+            ans = max(ans,max_cache[i]);
+        }
+        return ans;
     }
+    /*
+    int n = nums.size();
+        int max_array[n];
+        max_array[0]=nums[0];
+        for(int i=1;i<n;i++){
+            max_array[i] = max(max_array[i-1]+nums[i],nums[i]);
+        }
+        int ans = max_array[0];
+        for(int i=1;i<n;i++){
+            ans = max(ans,max_array[i]);
+        }
+        return ans;*/
 };
