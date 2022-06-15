6a9c243d553ab4133cef7013f91e005f0c5a0a87
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-06-15 14:29:52 +0530

    Time: 3 ms (73.86%), Space: 9.1 MB (71.69%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -7,17 +7,53 @@ public:
             nums1 = nums2;
             return;
         }
-        int i,k;
-        for(i=0;i<m;i++){
-             if(nums1[i]>nums2[0])
-                swap(nums1[i],nums2[0]);           
-            int first = nums2[0];
-            for(k=1;(k<n and nums2[k]<first);k++)
-                nums2[k-1] = nums2[k];
-            nums2[k-1] = first;
+        int gap = ceil((float)(n + m) / 2);
+        while(gap>0){
+            int i=0;
+            int j = gap;
+            while(j<(m+n)){
+                if(j<m and nums1[i]>nums1[j]){
+                    swap(nums1[i],nums1[j]);
+                }
+                else if(j>=m and i<m and nums1[i]>nums2[j-m]){
+                    swap(nums1[i],nums2[j-m]);                    
+                }else if(j>=m and i>=m and nums2[i-m]>nums2[j-m]){
+                    swap(nums2[i-m],nums2[j-m]);
+                }
+                i++;
+                j++;
+            }
+            if (gap == 1) {
+                 gap = 0;
+            } else {
+                gap = ceil((float) gap / 2);
+            }
         }
-        for(k=i;k<m+n;k++){
-            nums1[k] = nums2[k-m];
+        for(int k=m;k<n+m;k++){
+            nums1[k]=nums2[k-m];
         }
+        
     }
+    /*
+    int gap = ceil((float)(n + m) / 2);
+  while (gap > 0) {
+    int i = 0;
+    int j = gap;
+    while (j < (n + m)) {
+      if (j < n && ar1[i] > ar1[j]) {
+        swap(ar1[i], ar1[j]);
+      } else if (j >= n && i < n && ar1[i] > ar2[j - n]) {
+        swap(ar1[i], ar2[j - n]);
+      } else if (j >= n && i >= n && ar2[i - n] > ar2[j - n]) {
+        swap(ar2[i - n], ar2[j - n]);
+      }
+      j++;
+      i++;
+    }
+    if (gap == 1) {
+      gap = 0;
+    } else {
+      gap = ceil((float) gap / 2);
+    }
+  }*/
 };
