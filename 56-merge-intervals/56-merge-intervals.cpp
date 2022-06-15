300a6b6740dc2421dd37f2bb349bdbd6e96f763c
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-06-15 12:25:13 +0530

    Time: 89 ms (15.21%), Space: 21.7 MB (10.17%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -5,21 +5,21 @@ class Solution {
 public:
     bool isOverlapping(vector<int>&a,vector<int>&b){
         return a[1]>=b[0];
-    }
-    
+    }    
     vector<vector<int>> merge(vector<vector<int>>& nums) {
         int n = nums.size();
         vector<vector<int>> result;
         sort(nums.begin(),nums.end(),sortPair);
         result.push_back(nums[0]);
-        for(int i=1;i<n;i++){
-            if(isOverlapping(result[result.size()-1],nums[i])){
-                vector<int> temp = result[result.size()-1];//last stored element
-                temp[1] = max(nums[i][1],temp[1]);
+        for(auto it:nums){
+            vector<int> temp = result.back();
+            if(isOverlapping(temp,it)){
+                //last stored element
+                temp[1] = max(it[1],temp[1]);
                 result.pop_back();
                 result.push_back(temp);
             }else{
-                result.push_back(nums[i]);
+                result.push_back(it);
             }
         }
         return result;
