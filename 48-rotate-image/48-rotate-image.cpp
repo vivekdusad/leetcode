74b395c2fd62302762ce3b5c1cdc53f6c902077f
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-06-18 14:35:54 +0530

    Time: 5 ms (28.11%), Space: 7.1 MB (73.10%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
+    
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix[0].size();        
-        for(int i=0;i<n;i++)
-            for(int j=0;j<i;j++)
-                swap(matrix[i][j],matrix[j][i]);        
-        for(int i=0;i<n;i++)
-            reverse(matrix[i].begin(),matrix[i].end());
+        int n = matrix.size();
+        for(int i=0;i<(n+1)/2;i++){
+            for(int j=0;j<n/2;j++){
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+        
     }
-    /*
-    for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-        
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                cout<<matrix[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        
-        
-        //Reverse Rows of Matrix
-        for(int i=0;i<matrix.size();i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
-    */
+    
 };
