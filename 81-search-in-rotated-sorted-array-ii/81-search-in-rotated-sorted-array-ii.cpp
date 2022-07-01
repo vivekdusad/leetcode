073ab67c8d2b60d4feccf3090158fcadbee78b3f
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-07-01 12:38:43 +0530

    Time: 16 ms (17.69%), Space: 13.8 MB (91.80%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -7,34 +7,59 @@ public:
         
         return nums[start] == element;
     }
-    bool search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size()-1;
+//     bool search(vector<int>& nums, int target) {
+//         int low = 0;
+//         int high = nums.size()-1;
         
-        while(low<=high){
-            int mid = (low+high)>>1;
-            cout<<mid<<endl;
+//         while(low<=high){
+//             int mid = (low+high)>>1;
+//             cout<<mid<<endl;
+//             if(nums[mid]==target) return true;
+            
+//             bool isPivotArray = isInFirstArray(nums,low,nums[mid]);
+//             bool isTargetArray = isInFirstArray(nums,low,target);
+//             if(isPivotArray ^ isTargetArray){
+//                 //they are present on the diffrent side
+//                 if(isPivotArray){
+//                     low = mid+1;
+//                 }else{
+//                     high = mid-1;
+//                 }
+//             }else {
+//                 // if they are present on the diffrent side
+//                 if(target>nums[mid]){
+//                     low = mid+1;
+//                 }else{
+//                     high = mid-1;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+    int search(vector<int>& nums, int target) {
+        //low high
+        int l = 0;
+        int h = nums.size()-1;
+        while(l<=h){
+            int mid = (l+h)>>1;
             if(nums[mid]==target) return true;
-            if(isBinarySearchHelpful(nums,low,nums[mid])){
-                cout<<low<<" "<<nums[mid]<<endl;
-                low++;
+            if(isBinarySearchHelpful(nums,l,nums[mid])){
+                cout<<l<<" "<<nums[mid]<<endl;
+                l++;
                 continue;
             }
-            bool isPivotArray = isInFirstArray(nums,low,nums[mid]);
-            bool isTargetArray = isInFirstArray(nums,low,target);
-            if(isPivotArray ^ isTargetArray){
-                //they are present on the diffrent side
-                if(isPivotArray){
-                    low = mid+1;
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target and nums[mid]>=target){
+                    h = mid-1;
                 }else{
-                    high = mid-1;
+                    l = mid+1;
                 }
-            }else {
-                // if they are present on the diffrent side
-                if(target>nums[mid]){
-                    low = mid+1;
+            }
+            else if(nums[mid]<=nums[h]){
+                if(nums[mid]<=target and nums[h]>=target){
+                    l = mid+1;
                 }else{
-                    high = mid-1;
+                    h = mid-1;
                 }
             }
         }
