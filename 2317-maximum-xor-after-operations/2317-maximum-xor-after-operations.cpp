5082f2fcd7fe2d82694f5ef2d70f4ebeed3cdf5f
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-07-02 21:46:48 +0530

    Time: 168 ms (50.38%), Space: 48 MB (88.27%) - LeetHub

---
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maximumXOR(vector<int>& nums) {
+        int ans =0;
+        for(auto it:nums){
+            ans  |=it;
+        }
+        return ans;
+    }
+};
