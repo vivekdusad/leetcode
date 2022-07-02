ed1e2529cc22633ed80eb5822f384420c3cd9e8a
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-07-02 09:51:12 +0530

    Time: 90 ms (72.84%), Space: 32.4 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -2,36 +2,24 @@ const int mod =1e9+7;
 class Solution {
 public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
-        sort(horizontalCuts.begin(), horizontalCuts.end());
-        sort(verticalCuts.begin(), verticalCuts.end());
- 
-        // Insert the right bound h and w
-        // in their respective vectors
+        sort(horizontalCuts.begin(),horizontalCuts.end());
+        sort(verticalCuts.begin(),verticalCuts.end());
+        
+        int maxHorizontalGap = horizontalCuts[0];
+        int maxVerticalGap = verticalCuts[0];
+        
         horizontalCuts.push_back(h);
         verticalCuts.push_back(w);
-         
-          //Initialising both by first indexs,
-          //to consider first rectangle formed by
-          //respective horizontal and vertical cuts
-        int maxHorizontal = horizontalCuts[0];
-        int maxVertical = verticalCuts[0];
- 
-        // Find the maximum Horizontal Length possible
-        for (int i = 1; i < horizontalCuts.size(); i++) {
-            int diff
-                = horizontalCuts[i] - horizontalCuts[i - 1];
-            maxHorizontal = max(maxHorizontal, diff);
+        for(int i=1;i<horizontalCuts.size();i++){
+            int diff = horizontalCuts[i]-horizontalCuts[i-1];
+            maxHorizontalGap = max(maxHorizontalGap,diff);
         }
- 
-        // Find the maximum vertical Length possible
-        for (int i = 1; i < verticalCuts.size(); i++) {
-            int diff
-                = verticalCuts[i] - verticalCuts[i - 1];
-            maxVertical = max(maxVertical, diff);
+        
+         for(int i=1;i<verticalCuts.size();i++){
+            int diff = verticalCuts[i]-verticalCuts[i-1];
+            maxVerticalGap = max(maxVerticalGap,diff);
         }
- 
-        // Return the maximum area of rectangle
-        return (int)((long)maxHorizontal * maxVertical
-                     % mod);
+        
+        return (1L* maxVerticalGap*maxHorizontalGap)%mod;
     }
 };
