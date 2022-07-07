7d126ea12bad1c7f63af16631ee69bae9d4ad651
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-07-07 22:28:44 +0530

    Added solution - LeetHub

---
+++ Interleaved Strings - GFG/interleaved-strings.java
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+//initial code
+import java.util.*;
+import java.lang.*;
+class InterLeaveString
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t>0)
+        {
+            String a =sc.next();
+            String b = sc.next();
+            String c = sc.next();
+            Solution g=new Solution();
+        
+            System.out.println(g.isInterLeave(a,b,c)==true?1:0);
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*you are required to complete this method */
+class Solution {
+	 public boolean is_Interleave(String s1, int i, String s2, int j, String s3, int k, int[][] memo) {
+        if (i == s1.length()) {
+            return s2.substring(j).equals(s3.substring(k));
+        }
+        if (j == s2.length()) {
+            return s1.substring(i).equals(s3.substring(k));
+        }
+        if (memo[i][j] >= 0) {
+            return memo[i][j] == 1 ? true : false;
+        }
+        boolean ans = false;
+        if (s3.charAt(k) == s1.charAt(i) && is_Interleave(s1, i + 1, s2, j, s3, k + 1, memo)
+                || s3.charAt(k) == s2.charAt(j) && is_Interleave(s1, i, s2, j + 1, s3, k + 1, memo)) {
+            ans = true;
+        }
+        memo[i][j] = ans ? 1 : 0;
+        return ans;
+    }
+    public boolean isInterLeave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        int memo[][] = new int[s1.length()][s2.length()];
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return is_Interleave(s1, 0, s2, 0, s3, 0, memo);
+    }
+}
+
