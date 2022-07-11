df9ac4b6ffc5925ccc461f036f786417a6dec299
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-07-11 22:19:13 +0530

    Time: 8 ms (100.00%), Space: 11.8 MB (100.00%) - LeetHub

---
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -0,0 +1,22 @@
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+
+class Solution {
+public:
+   int fillCups(vector<int>& A) {
+        int mx = 0, sum = 0;
+        for(int& a: A) {
+            mx = max(a, mx);
+            sum += a;
+        }
+        return max(mx, (sum + 1) / 2);
+    }
+};
