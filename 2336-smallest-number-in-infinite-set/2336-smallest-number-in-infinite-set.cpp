e3b9cc5fb1543a1a5299d41bb84d92535f655e0e
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-07-11 22:41:04 +0530

    Time: 139 ms (100.00%), Space: 35.4 MB (100.00%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -0,0 +1,40 @@
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+
+class SmallestInfiniteSet {
+public:
+    int cur;
+    set<int> s;
+    SmallestInfiniteSet() {
+        cur=1;
+    }
+    
+    int popSmallest() {
+        if(s.size()){
+            int res=*s.begin(); s.erase(res);
+            return res;
+        }else{
+            cur+=1;
+            return cur-1;
+        }
+    }
+    
+    void addBack(int num) {
+        if(cur>num) s.insert(num);
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
