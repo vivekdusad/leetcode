1613e0bdde7fd916a22c6ee7e0dd887ce2289391
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-07-13 15:28:10 +0530

    Time: 53 ms (22.60%), Space: 13.5 MB (83.52%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,34 +1,44 @@
 class Solution {
-public:
-    vector<int>vis,col;
-    bool dfs(int v, int c, vector<vector<int>>& graph){
-        vis[v]=1;
-        col[v]=c;
-        for(int child:graph[v]){
-            if(vis[child]==0){
-                // here c^1 is for flipping 1 by 0 or 0 by 1, that is flip the current color
-                if(dfs(child,c^1,graph)==false) 
+private:
+    bool isBipartiteHelper(vector<vector<int>>& adj,vector<int> &colr,int node){
+        if(colr[node]==-1) colr[node] = 1;
+        
+        for(auto v:adj[node]){
+            if(colr[v]==-1){
+                colr[v] = 1-colr[node];
+                if(!isBipartiteHelper(adj,colr,v)) return false;
+            }else if(colr[v]==colr[node]) return false;
+        }
+        return true;
+    }
+    bool DfsHelper(vector<vector<int>> &graph,int s,vector<int> &visited,vector<int> &selectedSet){
+        visited[s] = 1;
+        for(auto v:graph[s]){
+            if(visited[v]==false){
+                selectedSet[v] = 1;
+                if(!DfsHelper(graph,v,visited,selectedSet)){
                     return false;
-            }
-            else{
-                if(col[v]==col[child])
+                }
+                selectedSet[v] = -1;
+                if(!DfsHelper(graph,v,visited,selectedSet)){
                     return false;
+                }
+                
+            }else if(selectedSet[v]==selectedSet[s]){
+                    return false;                
             }
         }
         return true;
     }
-    
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vis.resize(n);
-        col.resize(n);
-
-        for(int i=0;i<n;++i){
-            if(vis[i]==0 && dfs(i,0,graph)==false){ 
-                return false;
-            }
-        }
-        
-        return true;
+public:
+    bool isBipartite(vector<vector<int>>& adj) {
+        int V = adj.size();
+        vector<int> colr(V,-1);
+	    for(int i=0;i<V;i++){
+	        if(colr[i]==-1){
+	            if(!isBipartiteHelper(adj,colr,i)) return false;
+	        }
+	    }
+	    return true;
     }
 };
