155a7fb0d2cd06582d9e3c0467a5d40179bb3c66
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-07-14 09:44:09 +0530

    Time: 53 ms (53.15%), Space: 18 MB (18.59%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,41 +2,40 @@ class Solution {
 private:
     vector<int> rowNum = {-1,1,0,0};
     vector<int> colNum = {0,0,1,-1};
-    
+    vector<int> offset = {-1,0,1,0,-1};
     bool isSafe(vector<vector<char>> &grid,int m,int n,int row,int col){
         if(row>=0 and col>=0 and row<n and col<m and grid[row][col]=='1'){
             return true;
         }
         return false;
     }
-    void DfsRec(vector<vector<char>> &grid,vector<vector<int>> &visited,int &row,int &col){
-        visited[row][col] = 1;
-        int n = grid.size();
-        int m = grid[0].size();
-        for(int i=0;i<4;i++){
-            int r = row+rowNum[i];
-            int c = col+colNum[i];
-            
-            if(isSafe(grid,m,n,r,c) and visited[r][c]==0){
-                
-                DfsRec(grid,visited,r,c);
-            }
-        }
-    }
 public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> visited(n,vector<int>(m,0));
-        int noOfContComp =0;
-        for(int row=0;row<n;row++){
-            for(int col=0;col<m ; col++){
-                if(visited[row][col]==0 and grid[row][col]=='1'){
-                    noOfContComp++;
-                    DfsRec(grid,visited,row,col);
+        int noOfIslands =0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m ; j++){
+                if(grid[i][j]=='1'){
+                    grid[i][j] = '0';
+                    noOfIslands++;
+                    queue<pair<int,int>> todos;
+                    todos.push({i,j});
+                    while(!todos.empty()){
+                        auto todo = todos.front();
+                        todos.pop();
+                        for(int k=0;k<4;k++){
+                            int r = todo.first+offset[k];
+                            int c = todo.second+offset[k+1];
+                            if(isSafe(grid,m,n,r,c)){
+                                grid[r][c] ='0';
+                                 todos.push({r, c});
+                            }
+                        }
+                    }
                 }
             }
         }
-        return noOfContComp;
+        return noOfIslands; 
     }
 };
