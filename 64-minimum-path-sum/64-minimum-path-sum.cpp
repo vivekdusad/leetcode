2fef5773f539343a26b6eb69493d26d42071a16e
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-08-04 14:21:12 +0530

    Time: 13 ms (66.45%), Space: 10.1 MB (53.31%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -3,6 +3,33 @@ public:
 	int minPathSum(vector<vector<int>>& grid) {
 		int n = grid.size();
 		int m = grid[0].size();
+		vector<int> prev(m,0);
+
+		for(int i=0;i<n;i++){
+            vector<int> curr(m,0);
+			for(int j=0;j<m;j++){
+				if(i==0 && j==0)
+					curr[j] = grid[i][j];
+				else{
+					int left = 1e9, right=1e9;
+					if(i>0)
+						left = prev[j];
+					if(j>0)
+						right = curr[j-1];
+					curr[j] = grid[i][j] + min(left, right);
+				}
+			}
+            prev = curr;
+		}
+		return prev[m-1];
+	}
+};
+/*
+class Solution {
+public:
+	int minPathSum(vector<vector<int>>& grid) {
+		int n = grid.size();
+		int m = grid[0].size();
 		vector<vector<int>>dp(n, vector<int>(m, 0));
 
 		for(int i=0;i<n;i++){
@@ -22,4 +49,4 @@ public:
 		}
 		return dp[n-1][m-1];
 	}
-};
+};*/
