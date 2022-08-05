1552f78e759b0f9dd6ad3168c9a4c5488369cccc
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-08-05 11:29:11 +0530

    Time: 18 ms (35.72%), Space: 7.1 MB (50.13%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int s1 = s.size();
+        int s2 = p.size();
+        vector<vector<bool>>dp(s2+1,vector<bool>(s1+1));
+        for(int i=0;i<=s2;i++){
+            for(int j=0;j<=s1;j++){
+                if(i==0&&j==0){
+                    dp[i][j]=true;
+                }
+                else if(i==0){
+                    dp[i][j]=false;
+                }
+                else if(j==0){
+                    if(p[i-1] == '*'&&i-2>=0){
+                        dp[i][j]=dp[i-2][j];
+                    }
+                    else{
+                        dp[i][j]=false;
+                    }
+                }
+                else{
+                    if(p[i-1]=='.'){
+                        dp[i][j]=dp[i-1][j-1];
+                    }
+                    else if(p[i-1]=='*'&& i-2>=0 && p[i-2]=='.'){
+                        dp[i][j] = dp[i][j-1] ||dp[i-2][j]; 
+                    }
+                    else if(p[i-1]=='*'){
+                        if(i-2>=0 && p[i-2]==s[j-1]){
+                            if(dp[i][j-1]||(i-2>=0 && dp[i-2][j])){
+                                dp[i][j]=true;
+                            }
+                            else{
+                                dp[i][j] = false;
+                            }
+                        }
+                        else{
+                            if(i-2>=0 && dp[i-2][j]){
+                                dp[i][j]=true;
+                            }
+                            else{
+                                dp[i][j] = false;
+                            }
+                        }
+                    }
+                    else if(p[i-1] == s[j-1] && dp[i-1][j-1]){
+                        dp[i][j]=true;
+                    }
+                    else{
+                        dp[i][j]=false;
+                    }
+                }
+            }
+        }
+        return dp[s2][s1];
+    }
+};
