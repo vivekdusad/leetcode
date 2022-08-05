9ddf2f5feec10f1e018a3304f869cf968eff2e1f
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-08-05 11:35:03 +0530

    Time: 12 ms (45.84%), Space: 7 MB (63.03%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -6,12 +6,8 @@ public:
         vector<vector<bool>>dp(s2+1,vector<bool>(s1+1));
         for(int i=0;i<=s2;i++){
             for(int j=0;j<=s1;j++){
-                if(i==0&&j==0){
-                    dp[i][j]=true;
-                }
-                else if(i==0){
-                    dp[i][j]=false;
-                }
+                if(i==0&&j==0) dp[i][j]=true;
+                else if(i==0) dp[i][j]=false;
                 else if(j==0){
                     if(p[i-1] == '*'&&i-2>=0){
                         dp[i][j]=dp[i-2][j];
@@ -21,33 +17,15 @@ public:
                     }
                 }
                 else{
-                    if(p[i-1]=='.'){
+                    if(p[i-1]=='.' or p[i-1] == s[j-1]){
                         dp[i][j]=dp[i-1][j-1];
-                    }
-                    else if(p[i-1]=='*'&& i-2>=0 && p[i-2]=='.'){
-                        dp[i][j] = dp[i][j-1] ||dp[i-2][j]; 
-                    }
-                    else if(p[i-1]=='*'){
-                        if(i-2>=0 && p[i-2]==s[j-1]){
-                            if(dp[i][j-1]||(i-2>=0 && dp[i-2][j])){
-                                dp[i][j]=true;
-                            }
-                            else{
-                                dp[i][j] = false;
-                            }
-                        }
-                        else{
-                            if(i-2>=0 && dp[i-2][j]){
-                                dp[i][j]=true;
-                            }
-                            else{
-                                dp[i][j] = false;
-                            }
-                        }
-                    }
-                    else if(p[i-1] == s[j-1] && dp[i-1][j-1]){
-                        dp[i][j]=true;
-                    }
+                    }                    
+                    else if(p[i-1]=='*' and i-2>=0){
+                        dp[i][j] = dp[i-2][j];
+                        if(p[i-2]==s[j-1] or p[i-2]=='.'){
+                            dp[i][j] = dp[i][j] or  dp[i][j-1];
+                        }                        
+                    }                    
                     else{
                         dp[i][j]=false;
                     }
