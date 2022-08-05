1d64b9e26697e5ca3583a949b2e12efbe44981df
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-08-05 09:39:50 +0530

    Time: 79 ms (62.35%), Space: 8 MB (80.69%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -11,34 +11,29 @@ class Solution {
 public:
     bool isMatch(string S2, string S1) {
          int n = S1.size();
-          int m = S2.size();
+         int m = S2.size();
 
-          vector < vector < bool >> dp(n + 1, vector < bool > (m+1, false));
-
-          dp[0][0] = true;
-
-          for (int j = 1; j <= m; j++) {
-            dp[0][j] = false;
+        vector < bool > prev(m+1, false);
+        
+        prev[0] = true;
+         for (int j = 1; j <= m; j++) {
+            prev[j] = false;
           }
+        
           for (int i = 1; i <= n; i++) {
-            dp[i][0] = isAllStars(S1, i);
-          }
-
-          for (int i = 1; i <= n; i++) {
+            vector<bool> curr(m+1, false);
+            curr[0] = isAllStars(S1, i);
             for (int j = 1; j <= m; j++) {
-
               if (S1[i - 1] == S2[j - 1] || S1[i - 1] == '?')
-                dp[i][j] = dp[i - 1][j - 1];
-
+                curr[j] = prev[j - 1];
               else {
                 if (S1[i - 1] == '*')
-                  dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-
-                else dp[i][j] = false;
+                  curr[j] = prev[j] | curr[j - 1];
+                else curr[j] = false;
               }
             }
+            prev = curr;
           }
-
-          return dp[n][m];
+        return prev[m];
     }
 };
