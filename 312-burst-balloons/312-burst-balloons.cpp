04c0c2faa1b40603e6aa61d63483a045efd6e07c
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-08-07 15:43:58 +0530

    Time: 391 ms (85.00%), Space: 10.3 MB (52.71%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -14,7 +14,18 @@ public:
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return maxCoinsHelper(nums,1,n,dp);
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        
+        for(int i=n;i>=1;i--){
+            for(int j =i;j<=n;j++){
+                int max_coins = INT_MIN;
+                for(int k=i;k<=j;k++){
+                    int coins = nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j];
+                    max_coins = max(coins,max_coins);
+                }
+                dp[i][j] =  max_coins;
+            }
+        }
+        return dp[1][n];
     }
 };
