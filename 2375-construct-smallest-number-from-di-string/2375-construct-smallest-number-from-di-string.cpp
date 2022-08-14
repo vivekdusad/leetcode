83e185589eb2a24d9ec3efe698ae428097e359ad
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-08-14 15:04:21 +0530

    Time: 23 ms (55.00%), Space: 5.9 MB (65.00%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
+    int dfs(string& p, int i, int n, int mask) {
+    if (i > p.size())
+        return n;
+    int res = INT_MAX, last = n % 10, increment = i == 0 || p[i - 1] == 'I';
+    for (int d = 1; d <= 9; ++d)
+        if ((mask & (1 << d)) == 0 && d > last == increment)
+            res = min(res, dfs(p, i + 1, n * 10 + d, mask + (1 << d)));               
+    return res;
+}
     string smallestNumber(string pattern) {
-        string res = "",st = "";
-        int n = pattern.size();
-        for(int i=0;i<=n;i++){
-            st.push_back(i+'1');
-            if(i==n or pattern[i]=='I'){
-                while(!st.empty()){
-                    res.push_back(st.back());
-                    st.pop_back();
-                }
-            }
-        }
-        return res;
+        return to_string(dfs(pattern,0,0,0));
     }
 };
