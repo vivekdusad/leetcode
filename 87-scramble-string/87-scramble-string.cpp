5b2347c9de5050c7c35bbaa589e6432f5a366606
VIVEK KHANDELWAL <70028516+vivekdusad@users.noreply.github.com>
2022-08-14 20:57:28 +0530

    Time: 853 ms (5.06%), Space: 100.8 MB (5.03%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -1,84 +1,34 @@
 class Solution {
-    
-    unordered_map<string,bool> mp;
-   public:
-    bool isScramble(string s1, string s2) {
-        //base cases
+    string generateKey(string s1,string s2){
+        return s1+"__"+s2;
+    }
+   
+    bool isScrambleUtil(string s1,string s2,unordered_map<string,bool> &cache){
+        int n = s1.length();
+        if(s1 == s2) return true;
+        if(s1=="" or s2=="") return false;
         
-        int n = s1.size();
-        
-        //if both string are not equal in size
-        if(s2.size()!=n)
-            return false;
-        
-        //if both string are equal
-        if(s1==s2)
-         return true;   
-        
-            
-        
-        //if code is reached to this condition then following this are sure:
-        //1. size of both string is equal
-        //2.  string are not equal
-        //so size is equal (where size==1) and they are not equal then obviously false
-        //example 'a' and 'b' size is equal ,string are not equal
-        if(n==1)
-            return false;
-        
-        string key = s1+" "+s2;
-        
-		//check if this problem has already been solved
-        if(mp.find(key)!=mp.end())
-            return mp[key];
-        
-        //for every iteration it can two condition 
-        //1.we should proceed without swapping
-        //2.we should swap before looking next
-        for(int i=1;i<n;i++)
-        {
-
-            //ex of without swap: gr|eat and rg|eat
-            bool withoutswap = (
-                //left part of first and second string
-                isScramble(s1.substr(0,i),s2.substr(0,i)) 
-                
-                &&
-                
-                //right part of first and second string;
-                isScramble(s1.substr(i),s2.substr(i))
-                );
-            
-            
-            
-            //if without swap give us right answer then we do not need 
-            //to call the recursion withswap
-            if(withoutswap)
-                return true;
-            
-            //ex of withswap: gr|eat  rge|at
-			//here we compare "gr" with "at" and "eat" with "rge"
-            bool withswap = (
-                //left part of first and right part of second 
-                isScramble(s1.substr(0,i),s2.substr(n-i)) 
-                
-                &&
-                
-                //right part of first and left part of second
-                isScramble(s1.substr(i),s2.substr(0,n-i)) 
-                );
-            
-            
-            
-            //if withswap give us right answer then we return true
-            //otherwise the for loop do it work
-            if(withswap)
-                return true;
-            //we are not returning false in else case 
-            //because we want to check further cases with the for loop
+        string key = generateKey(s1,s2);    
+        //used the cached Result
+        if(cache.find(key)!=cache.end()){
+            return cache[key];
         }
-        
-        
-        return mp[key] = false;
-        
+        //Driver Code
+        bool flag = false;
+        for(int i=1;i<n;i++){
+            //case 1
+            bool case1 =isScrambleUtil(s1.substr(0,i),s2.substr(n-i),cache)==true && isScrambleUtil(s1.substr(i,n-i),s2.substr(0,n-i),cache)==true;
+            //case 2
+            bool case2 = isScrambleUtil(s1.substr(0,i),s2.substr(0,i),cache) && isScrambleUtil(s1.substr(i),s2.substr(i),cache);
+            
+            if(case1 or case2) flag = true;
+        }
+        //Puting the result in the cache
+        return cache[key] =  flag;
+    }
+public:
+    bool isScramble(string s1, string s2) {
+        unordered_map<string,bool> cache;
+        return isScrambleUtil(s1,s2,cache);
     }
 };
